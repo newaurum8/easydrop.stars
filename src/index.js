a9afa8c895ff22cf6acb88f7d699bfb62e9043db
newaurum8 <newaurum1999@gmail.com>
2025-10-16 02:36:08 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,12 +3,20 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { AppProvider } from './context/AppContext';
 import './style.css';
+import { TonConnectUIProvider } from '@tonconnect/ui-react';
+
+// Полифилл, необходимый для работы крипто-библиотек в браузере
+import { Buffer } from 'buffer';
+window.Buffer = Buffer;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <AppProvider>
-      <App />
-    </AppProvider>
+    {/* Оборачиваем все приложение в провайдер TON Connect */}
+    <TonConnectUIProvider manifestUrl="https://easydrop-stars-1.onrender.com/tonconnect-manifest.json">
+      <AppProvider>
+        <App />
+      </AppProvider>
+    </TonConnectUIProvider>
   </React.StrictMode>
 );
