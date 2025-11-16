8bd07ea129cccb365a531caa01a67d89074b6709
newaurum8 <newaurum1999@gmail.com>
2025-11-17 00:08:30 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,37 +7,7 @@ import ProfilePage from './pages/ProfilePage';
 import HistoryPage from './pages/HistoryPage';
 import CasePage from './pages/CasePage';
 import LeadersPage from './pages/LeaderPage';
-import AdminPage from './pages/AdminPage'; // <-- Импорт остается
-
-function App() {
-  return (
-    <Router>
-      <Routes>
-        {/* --- Маршруты ВНУТРИ Layout (с шапкой и меню) --- */}
-        <Route path="/" element={<Layout><MainPage /></Layout>} />
-        <Route path="/upgrade" element={<Layout><UpgradePage /></Layout>} />
-        <Route path="/profile" element={<Layout><ProfilePage /></Layout>} />
-        <Route path="/history" element={<Layout><HistoryPage /></Layout>} />
-        <Route path="/leaders" element={<Layout><LeadersPage /></Layout>} />
-        <Route path="/case/:caseId" element={<Layout><CasePage /></Layout>} />
-        
-        {/* --- Отдельный маршрут БЕЗ Layout --- */}
-        <Route path="/admin" element={<AdminPage />} /> 
-      </Routes>
-    </Router>
-  );
-}
-
-export default App;import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Layout from './components/Layout';
-import MainPage from './pages/MainPage';
-import UpgradePage from './pages/UpgradePage';
-import ProfilePage from './pages/ProfilePage';
-import HistoryPage from './pages/HistoryPage';
-import CasePage from './pages/CasePage';
-import LeadersPage from './pages/LeaderPage';
-import AdminPage from './pages/AdminPage'; // <-- Импорт остается
+import AdminPage from './pages/AdminPage';
 
 function App() {
   return (
